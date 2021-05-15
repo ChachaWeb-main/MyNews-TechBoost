bb3438bae1c118b480e04a4230c1869b5805cf73
daisuke <tyatya1702@gmail.com>
2021-05-15 06:00:04 +0000

    No20_P/L11_task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>プロフィール作成</h1>
-        <h2>これはPHP/Laravel10の課題４です</h2>
-    </body>
-</html
+@extends('layouts.profile')
+
+
+@section('title', 'Myプロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
