d85e24a69bf60ed85b975746ae0c83332bf2883a
daisuke <tyatya1702@gmail.com>
2021-05-22 07:14:27 +0000

    No22_P/L13_task

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -10,5 +10,11 @@ class NewsController extends Controller
     // 以下を追記
     public function add(){
         return view('admin.news.create');
-    } 
+    }
+    
+    // 以下を追記
+    public function create(Request $request){
+        // admin/news/createにリダイレクトする
+        return redirect('admin/news/create');
+    }
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -9,6 +9,61 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create')}}" method="post" entype="multipart/form-data">
+                    
+                    @if (count($errors) > 0)
+                      <ul>
+                          @foreach($errors->all() as $e)
+                            <li>{{ $e }}</li>
+                          @endforeach    
+                      </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名 (Name)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">性別 (Gender)</label>
+                        <div class="col-md-10">
+                            <!--パターンA-->
+                            <!--<select name="pets" id="pet-select">-->
+                            <!--    <option value="">--選択してください--</option>-->
+                            <!--    <option value="男性">男性(Male)</option>-->
+                            <!--    <option value="女性">女性(Female)</option>-->
+                            <!--</select>    -->
+                            
+                            <!--パターンB-->
+                            <div>
+                              <input type="radio" id="男性 (Male)" name="drone" value="男性 (Male)" checked>
+                              <label for="男性 (Male)">男性 (Male)</label>
+                            </div>
+                            
+                            <div>
+                              <input type="radio" id="女性 (Female)" name="drone" value="女性 (Female)">
+                              <label for="女性 (Female)">女性 (Female)</label>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味 (Hobby)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介 (Introduction)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="15">{{ old('body') }}</textarea>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form> 
             </div>
         </div>
     </div>
