c8c256e157125c9f8a186caa4a3c794400e44fff
daisuke <tyatya1702@gmail.com>
2021-05-25 07:42:43 +0000

    N023_P/L14_task

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでNews Modelが扱えるようになる(P/L 14)
+use App\News;   
+
 class NewsController extends Controller
 {
     // 以下を追記
@@ -12,8 +15,29 @@ class NewsController extends Controller
         return view('admin.news.create');
     }
     
-    // 以下を追記
+    // 以下を追記(P/L 13)
     public function create(Request $request){
+        // 以下を追記、Varidationを行う(P/L 14)
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
