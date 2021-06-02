f0315db03b280b6d404ec235129173d8ee1eaa52
daisuke <tyatya1702@gmail.com>
2021-06-02 23:35:47 +0000

    P/L17カリキュラム

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 // 以下を追記することでNews Modelが扱えるようになる(P/L 14)
-use App\News;   
+use App\News;
+
+// 以下を追記(P/L17)
+use App\History;
+
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -92,6 +97,12 @@ class NewsController extends Controller
       unset($news_form['_token']);
       // 該当するデータを上書きして保存する
       $news -> fill($news_form) -> save();
+      
+      // 以下を追記(P/L17)
+      $history = new History;
+      $history -> news_id = $news -> id;
+      $history -> edited_at = Carbon::now();
+      $history -> save();
       
       return redirect('admin/news');
   }
